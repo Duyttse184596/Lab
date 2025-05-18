23c59c6c6e3df8f65e7cb0d4f2d5dc1b547aa370
Duy <duyttse184596@fpt.edu.vn>
2025-05-18 14:40:40 +0700

    Add project files.

---
+++ AutomobileWPFApp/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿using AutomobileLibrary.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+using System;
+using System.Windows;
+
+namespace AutomobileWPFApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private ServiceProvider serviceProvider;
+        public App()
+        {
+            ServiceCollection services = new ServiceCollection();
+            ConfigureServices(services);
+            serviceProvider = services.BuildServiceProvider();
+        }
+        private void ConfigureServices(ServiceCollection services)
+        {
+            services.AddSingleton<ICarRepository, CarRepository>();
+            services.AddSingleton<MainWindow>();
+
+        }
+
+        private void OnStartup(object sender, EventArgs e)
+        {
+            try
+            {
+                var window = serviceProvider.GetService<MainWindow>();
+                if (window == null)
+                {
+                    MessageBox.Show("MainWindow is null from DI");
+                    return;
+                }
+                window.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Startup Error");
+            }
+        }
+    }
+
+}
---
+++ AutomobileWPFApp/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using AutomobileLibrary.DataAccess;
+using AutomobileLibrary.Repository;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AutomobileWPFApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ICarRepository carRepository;
+        public MainWindow(ICarRepository repository)
+        {
+            InitializeComponent();
+            if (repository == null)
+            {
+                MessageBox.Show("ICarRepository is NULL — DI failed");
+            }
+
+            carRepository = repository;
+
+        }
+
+        private Car GetCarObject()
+        {
+            Car car = null;
+            try
+            {
+                car = new Car
+                {
+                    CarId = int.Parse(txtCarId.Text),
+                    CarName = txtCarName.Text,
+                    Manufacturer = txtManufacturer.Text,
+                    Price = decimal.Parse(txtPrice.Text),
+                    ReleasedYear = int.Parse(txtReleasedYear.Text),
+                };
+            }
+            catch(Exception ex) { 
+                MessageBox.Show(ex.Message, "Get Car");    
+            }
+            return car;
+        }
+
+        public void LoadCarList()
+        {
+            lvCars.ItemsSource = carRepository.GetCars();
+        }
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                LoadCarList();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Load car list");
+            }
+        }
+
+        private void btnInsert_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Car car = GetCarObject();
+                carRepository.InsertCar(car);
+                LoadCarList();
+                MessageBox.Show($"{car.CarName} insert successfully", "Insert car");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Insert car ");
+            }
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Car car = GetCarObject();
+                carRepository.UpdateCar(car);
+                LoadCarList();
+                MessageBox.Show($"{car.CarName} update successfully", "Update car");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Update car ");
+            }
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Car car = GetCarObject();
+                carRepository.DeleteCar(car);
+                LoadCarList();
+                MessageBox.Show($"{car.CarName} delete successfully", "Delete car");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Delete car ");
+            }
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e) => Close();
+
+    }
+}
